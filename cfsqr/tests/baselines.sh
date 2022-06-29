d0aa25f9e111eab7aede60c1842eac873622e917
Burooj Armagaan <armgaan009@gmail.com>
2022-06-29 14:19:52 +0530

    Baselines - Added call for computing treegrids baselines. - Computed new outputs.

--- cfsqr/tests/baselines.sh
+++ cfsqr/tests/baselines.sh
@@ -3,17 +3,25 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate cfsqr
 
 echo "<----- BA-SHAPES ALPHA=0.0 ----->"
-python tests/tests.py outputs/bashapes/bashapes-alp_0.0-1653544735
+python tests/tests.py outputs/bashapes/bashapes-alp_0.0-1656483294
 echo
 
 echo "<----- BA-SHAPES ALPHA=0.6 ----->"
-python tests/tests.py outputs/bashapes/bashapes-alp_0.6-1653548599
+python tests/tests.py outputs/bashapes/bashapes-alp_0.6-1656486154
 echo
 
 echo "<----- TREE-CYCLES ALPHA=0.0 ----->"
-python tests/tests.py outputs/treecycles/treecycles-alp_0.0-1653386395
+python tests/tests.py outputs/treecycles/treecycles-alp_0.0-1656482998
 echo
 
 echo "<----- TREE-CYCLES ALPHA=0.6 ----->"
-python tests/tests.py outputs/treecycles/treecycles-alp_0.6-1653386374
+python tests/tests.py outputs/treecycles/treecycles-alp_0.6-1656483099
+echo
+
+echo "<----- TREE-GRIDS ALPHA=0.0 ----->"
+python tests/tests.py outputs/treegrids/treegrids-alp_0.0-1656491030
+echo
+
+echo "<----- TREE-GRIDS ALPHA=0.6 ----->"
+python tests/tests.py outputs/treegrids/treegrids-alp_0.6-1656491434
 echo

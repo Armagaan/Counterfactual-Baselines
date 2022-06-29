8e0a4d8ad3e5780b50eeaba5ff809f7da24af84f
Burooj Armagaan <armgaan009@gmail.com>
2022-06-29 22:25:24 +0530

    updated paths to new outputs

--- cfsqr/tests/baselines.sh
+++ cfsqr/tests/baselines.sh
@@ -3,25 +3,25 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate cfsqr
 
 echo "<----- BA-SHAPES ALPHA=0.0 ----->"
-python tests/tests.py outputs/bashapes/bashapes-alp_0.0-1656483294
+python tests/tests.py outputs/bashapes/bashapes-alp_0.0-1656513715
 echo
 
 echo "<----- BA-SHAPES ALPHA=0.6 ----->"
-python tests/tests.py outputs/bashapes/bashapes-alp_0.6-1656486154
+python tests/tests.py outputs/bashapes/bashapes-alp_0.6-1656516517
 echo
 
 echo "<----- TREE-CYCLES ALPHA=0.0 ----->"
-python tests/tests.py outputs/treecycles/treecycles-alp_0.0-1656482998
+python tests/tests.py outputs/treecycles/treecycles-alp_0.0-1656512240
 echo
 
 echo "<----- TREE-CYCLES ALPHA=0.6 ----->"
-python tests/tests.py outputs/treecycles/treecycles-alp_0.6-1656483099
+python tests/tests.py outputs/treecycles/treecycles-alp_0.6-1656512328
 echo
 
 echo "<----- TREE-GRIDS ALPHA=0.0 ----->"
-python tests/tests.py outputs/treegrids/treegrids-alp_0.0-1656491030
+python tests/tests.py outputs/treegrids/treegrids-alp_0.0-1656511167
 echo
 
 echo "<----- TREE-GRIDS ALPHA=0.6 ----->"
-python tests/tests.py outputs/treegrids/treegrids-alp_0.6-1656491434
+python tests/tests.py outputs/treegrids/treegrids-alp_0.6-1656511427
 echo

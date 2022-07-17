bb839f07255641e7e6878c657b398cb85b02ff05
Burooj Armagaan <armgaan009@gmail.com>
2022-07-17 11:49:11 +0530

    - [BUG FIX]: Mismatched keys while calculating fidelity. - Removed redundant jupyter notebook. - Removed old baseline script.

--- Gem/tests/baselines.py
+++ Gem/tests/baselines.py
@@ -1,5 +1,4 @@
-"""Compute baselines for node-classification."""
-
+# Baselines
 from collections import defaultdict
 import os
 import pickle
@@ -12,23 +11,24 @@ sys.path.append("gnnexp")
 from models import GCNSynthetic
 
 if len(sys.argv) != 3:
-    print("\nUSAGE: python tests/baselines.py [DATASET] [EVAL-MODE]")
-    print("DATASET: syn1, syn4, syn5")
-    print("EVAL-MODE: train, eval")
+    print("\nUSAGE: python tests/baselines.py [DATASET] [EVALMODE]")
+    print("[DATASET]: syn1, syn4, syn5")
+    print("[EVALMODE]: train, eval")
     exit(1)
 
+## ===== CONSTANTS =====
 DATASET = sys.argv[1]
 EVAL = sys.argv[2]
 
 
-## ===== Data =====
-# Extracted subadjacency matrices.
-#todo: automate the output folder path usage.
+## ===== DATA =====
+# The extracted subadjacency matrices.
+#todo: Automate supply of the output folder.
 if DATASET == 'syn1':
     with open("output/syn1/1657970490/original_sub_data.pkl", "rb") as file:
         sub_data = pickle.load(file)
 elif DATASET == 'syn4':
-    with open("output/syn4/1657890667/original_sub_data.pkl", "rb") as file:
+    with open("output/syn4/1658036723/original_sub_data.pkl", "rb") as file:
         sub_data = pickle.load(file)
 
 sub_labels = dict()
@@ -44,7 +44,7 @@ for filename in os.listdir(PATH):
     explanations[int(filename[4:7])] = pd.read_csv(f"{PATH}/{filename}", header=None).to_numpy()
 
 
-## ===== Model =====
+## ===== MODEL =====
 ckpt = torch.load(f"data/{DATASET}/eval_as_{EVAL}.pt")
 cg_dict = ckpt["cg"]
 input_dim = cg_dict["feat"].shape[2] 
@@ -52,7 +52,6 @@ num_classes = cg_dict["pred"].shape[2]
 feat = torch.from_numpy(cg_dict["feat"]).float()
 adj = torch.from_numpy(cg_dict["adj"]).float()
 label = torch.from_numpy(cg_dict["label"]).long()
-
 with open(f"tests/prog_args_{DATASET}.pkl", "rb") as file:
     prog_args = pickle.load(file)
 
@@ -67,42 +66,41 @@ model.load_state_dict(ckpt["model_state"])
 model.eval()
 
 
-## ===== Predictions =====
+## ===== PREDICTIONS =====
 predictions = dict()
 for node in explanations:
     sub_adj = sub_data[node]['org_adj'] - torch.Tensor(explanations[node]).unsqueeze(0)
-    sub_adj = sub_adj + torch.eye(sub_adj.size(-1))
     new_idx = sub_data[node]['node_idx_new']
     pred_proba = model(
         sub_data[node]['sub_feat'],
         sub_adj
     ).squeeze(0)
     predictions[node] = int(torch.argmax(pred_proba[new_idx]))
-predictions = torch.Tensor(list(predictions.values()))
-labels = torch.Tensor(list(sub_labels.values()))
 
 
-## ===== Fidelity =====
-fidelity = 1 - torch.sum(predictions != labels)/labels.size(0)
-print("\n====================")
+## ===== FIDELITY =====
+misclassifications = 0
+for node in predictions:
+    if predictions[node] != sub_labels[node]:
+        misclassifications += 1
+fidelity = 1 - misclassifications/len(predictions)
+print("\n===============")
 print(f"Fidelity: {fidelity:.2f}")
 
 
-## ===== Per label fidelity =====
+## ===== PER-LABEL FIDELITY =====
 per_label_mismatches = defaultdict(int)
-for label, pred in zip(labels, predictions):
-    if label == pred:
-        continue
-    per_label_mismatches[int(label)] += 1
+for node in predictions:
+    label = sub_labels[node]
+    if predictions[node] != label:
+        per_label_mismatches[int(label)] += 1
+labels, label_counts = torch.Tensor(list(sub_labels.values())).unique(return_counts=True)
 nodes_per_label = {
-    int(key):int(val) for key, val in zip(
-        labels.unique(return_counts=True)[0],
-        labels.unique(return_counts=True)[1]
-    )
+    int(key):int(val) for key, val in zip(labels, label_counts)
 }
-print("\n====================")
-print("Per label fidelity:\n")
+print("\n===============")
+print("Per label fidelity:")
 for label in per_label_mismatches:
-    print(f"Label-{label}", end=': ')
+    print(f"Label-{label}", end=": ")
     print(f"{1 - per_label_mismatches[label]/nodes_per_label[label]:.2f}")
     print()

3f1b07a4a2cbff7347c61141e8a5ecbe02bb8deb
Burooj Armagaan <armgaan009@gmail.com>
2022-07-18 10:13:07 +0530

    - Had missed adding preds into the checkpoint. Added code for that in the notebook and the script.

--- Gem/tests/create_checkpoint.py
+++ Gem/tests/create_checkpoint.py
@@ -6,6 +6,9 @@ import sys
 
 import torch
 
+sys.path.append("gnnexp")
+from models import GCNSynthetic
+
 if len(sys.argv) != 2:
     print("\nUSAGE: python tests/create_checkpoint.py [DATASET]")
     print("[DATASET]: syn1, syn4, syn5")
@@ -26,9 +29,32 @@ with torch.no_grad():
     features = torch.tensor(cg_dict["feat"][0], dtype=torch.float)
     num_class = max(label)+1
 
+with open(F"tests/prog_args_{DATASET}.pkl", "rb") as file:
+    prog_args = pickle.load(file)
+
+
+## ===== Model =====
+model = GCNSynthetic(
+    nfeat=input_dim,
+    nhid=prog_args.hidden_dim,
+    nout=prog_args.output_dim,
+    nclass=num_class,
+    dropout=0.0,
+)
+
 
 ## ===== CFGNN MODEL WEIGHTS =====
 state_dict_cfgnn = torch.load(f"cfgnn_model_weights/gcn_3layer_{DATASET}.pt")
+
+
+## ===== Preds =====
+model.load_state_dict(state_dict_cfgnn)
+model.eval()
+preds = model(
+    torch.from_numpy(cg_dict['feat']).float(),
+    torch.from_numpy(cg_dict['adj']).float()
+)
+cg_dict['pred'] = preds
 
 
 ## ===== EVAL SET =====
@@ -42,7 +68,6 @@ elif DATASET == "syn4":
 elif DATASET == "syn5":
     KEY = "syn5/tree-grids"
 
-
 # Our eval set as part of the training set
 train_set_indices = [range(label.shape[0])]
 test_set_indices = eval_set[KEY]
@@ -52,7 +77,6 @@ ckpt["cg"]["test_idx"] = test_set_indices
 ckpt["model_state"] = state_dict_cfgnn
 os.makedirs(f"data/{DATASET}", exist_ok=True)
 torch.save(ckpt, f"data/{DATASET}/eval_as_train.pt")
-
 
 # Our eval set as the validation set
 train_set_indices = [i for i in range(label.shape[0]) if i not in eval_set[KEY]]

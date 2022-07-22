- New outputs. - Computed baselines with new weights. - Fixed fidelity calculation.

cfsqr/models/explainer_models.py
@@ -81,7 +81,7 @@ class GraphExplainerEdge(torch.nn.Module):
         # train explainer
         optimizer = torch.optim.Adam(explainer.parameters(), lr=self.args.lr, weight_decay=0)
         explainer.train() # ! Calling this line set the base_model in training mode as well.
-        explainer.base_model.training = False # * manually set training off.
+        # explainer.base_model.training = False # * manually set training off.
 
         for epoch in range(self.args.num_epochs):
             explainer.zero_grad()

cfsqr/scripts/exp_graph.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         device = 'cpu'
     
     base_model = GCNGraph_Mutag(G_dataset.feat_dim, 128).to(device)
-    state_dict = torch.load("graph_classification_model_weights/mutag_weights.pt")
+    state_dict = torch.load("graph_classification_model_weights/new_mutag_weights.pt")
     base_model.load_state_dict(state_dict)
     #  fix the base model
     for param in base_model.parameters():

cfsqr/scripts/exp_graph_nci1.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     else:
         device = 'cpu'
     base_model = GCNGraph(G_dataset.feat_dim, 128).to(device)
-    state_dict = torch.load("graph_classification_model_weights/nci1_weights.pt")
+    state_dict = torch.load("graph_classification_model_weights/new_nci1_weights.pt")
     base_model.load_state_dict(state_dict)
     #  fix the base model
     for param in base_model.parameters():
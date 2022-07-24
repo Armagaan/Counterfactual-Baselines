2b401c5325c2910367f61e31e2842b5dd4b02b95
Samidha09 <samidha.verma1996@gmail.com>
2022-07-24 19:17:58 +0530

    - Bash script for computing baselines in one line.

---
+++ Gem/tests/baselines_graph.py
@@ -0,0 +1,121 @@
+# Graph Classification Baselines
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import torch
+
+sys.path.append("./")
+from gnnexp.models import GNN_Custom_Graph
+
+def usage():
+    print("\nUSAGE: python tests/baselines_grpah.py [DATASET] [EVALMODE]")
+    print("[DATASET]: Mutagenicity, NCI1, IsCyclic")
+    print("[EVALMODE]: eval, train\n")
+    exit(1)
+
+if len(sys.argv) != 3:
+    usage()
+
+DATASET = sys.argv[1] # OPTIONS: Mutagenicity, NCI1, IsCyclic
+EVAL = sys.argv[2] # OPTIONS: eval, train
+if DATASET not in ["Mutagenicity", "NCI1", "IsCyclic"]:
+    print("INVALID DATASET!")
+    usage()
+if EVAL not in ["eval", "train"]:
+    print("INVALID EVALMODE")
+    usage()
+#todo: MUTAG dataset is different from other baselines.
+
+if DATASET == 'NCI1':
+    EXPLANATION_FOLDER = "nci1_dc_top20"
+
+
+## ===== Data =====
+explanations = dict()
+PATH = f"explanation/{EXPLANATION_FOLDER}"
+for filename in os.listdir(PATH):
+    if 'label' not in filename:
+        continue
+    graph_idx = ''.join(filter(lambda i: i.isdigit(), filename))
+    explanations[int(graph_idx)] = pd.read_csv(f"{PATH}/{filename}", header=None).to_numpy()
+
+ckpt = torch.load(f"data/{DATASET}/eval_as_{EVAL}.pt")
+cg_dict = ckpt["cg"] # get computation graph
+input_dim = cg_dict["feat"].shape[2]
+
+
+## ===== Model =====
+model = GNN_Custom_Graph(in_features=input_dim, h_features=128)
+model.load_state_dict(ckpt["model_state"])
+model.eval()
+
+
+## ===== Predictions =====
+predictions = list()
+labels = list()
+for graph_idx in cg_dict['test_idx']:
+    feat = cg_dict["feat"][graph_idx, :].float().unsqueeze(0)
+    adj = cg_dict["adj"][graph_idx].float().unsqueeze(0) # - explanations[graph_idx]
+    label = cg_dict["label"][graph_idx].float().unsqueeze(0)
+    proba = model(feat, adj)
+    predictions.append(proba.round())
+    labels.append(label)
+predictions = torch.Tensor(predictions)
+labels = torch.Tensor(labels)    
+
+
+## ===== Fidelity & Explanation size =====
+top_indices = dict() # They are from the Upper Triangular part only.
+top_k = 20
+for graph_id, graph in explanations.items():
+    # triu: Upper Triangular
+    # abs: These are indices of the flattended version, not of the 2D version.
+    triu_abs_top_indices = (-np.triu(graph).flatten()).argsort()[:top_k]
+    index_rows = triu_abs_top_indices // graph.shape[0]
+    index_cols = triu_abs_top_indices % graph.shape[0]
+    triu_top_k_indices = [(r,c) for r,c in zip(index_rows, index_cols)]
+    top_indices[graph_id] = triu_top_k_indices
+
+explanation_size_list = list()
+cf_found_count = 0
+for graph_id, graph in explanations.items():
+    feat = cg_dict["feat"][graph_id, :].float().unsqueeze(0)
+    adj = cg_dict["adj"][graph_id].float().unsqueeze(0)
+    label = cg_dict["label"][graph_id].long().unsqueeze(0)
+    # get original prediction
+    original_prediction = model(feat, adj).round()
+    # set size_count to 0
+    # make a copy of the original adjacency.
+    size_count = 0
+    new_adj = adj.clone()
+    # work on correctly predicted label 1 nodes only.
+    if label == 1 and original_prediction == 1:
+        go = True
+    else:
+        go = False
+    if not go:
+        continue
+    for index in top_indices[graph_id]:
+        r1, c1 = index
+        r2, c2 = c1, r1 # for the lower triangular part.
+        # remove the edges
+        new_adj[0][r1, c1] = 0.0
+        new_adj[0][r2, c2] = 0.0
+        # make the prediction
+        new_prediction = model(feat, new_adj).round()
+        # increase size_count by 1.
+        size_count += 1
+        # if the label flipped: stop
+        if original_prediction != new_prediction:
+            cf_found_count += 1
+            explanation_size_list.append(size_count)
+            break
+
+fidelity = 1 - cf_found_count/len(explanations)
+exp_size_mean = np.mean(explanation_size_list)
+exp_size_std = np.std(explanation_size_list)
+
+print(f"\nFidelity: {fidelity:.2f}")
+print(f"Explanation size: mean={exp_size_mean:.2f}, std={exp_size_std:.2f}\n")
---
+++ Gem/tests/baselines_graph.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# * Run this script prior to running the notebooks.
+# * The output of this script is needed in those notebooks.
+
+# ! Run this script from the root directory of the repo.
+
+usage() {
+    # echo correct usage to cosole
+    echo
+    echo "USAGE: bash ${0} [-d DATASET] [-e EVALMDOE]" >&2
+    echo
+    echo "DATASET:  Dataset name. One of [Mutagenicity, NCI1, IsCyclic]"
+    echo "EVALMODE: Test on seen or unseen graphs. One of [eval, train]"
+    exit 1
+}
+
+# Check whether the required number of arguments are supplied.
+if [ ${#} != 4 ]; then
+    usage
+fi
+
+# Parse command line arguments
+while getopts "d:e:" OPTION; do
+    case ${OPTION} in
+        d) DATASET=${OPTARG} ;;
+        e) EVALMODE=${OPTARG} ;;
+        ?) usage ;;
+    esac
+done
+shift "$(( OPTIND - 1 ))"
+
+DATASETS="Mutagenicity NCI1 IsCyclic"
+EVALMODES="eval train"
+MIN=0.0
+MAX=1.0
+if [[ ! " ${DATASETS[*]} " =~ " ${DATASET} " ]]; then
+    echo "Invalid dataset"
+    usage
+elif [[ ! " ${EVALMODES[*]} " =~ " ${EVALMODE} " ]]; then
+    echo "Invalid evalmode"
+    usage
+fi
+
+# Activate the conda environment.
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate gem
+
+# Choose a script based on the supplied dataset.
+case ${DATASET} in
+    Mutagenicity)
+        python generate_ground_truth_graph_classification.py --dataset=Mutagenicity --output=mutag --graph-mode --top_k=20 --evalmode="$EVALMODE" &&
+        python explainer_gae_graph.py --distillation=mutag_top20 --output=mutag_top20 --dataset=Mutagenicity --gpu -b 128 --weighted --gae3 --loss=mse --early_stop --graph_labeling --train_on_positive_label --epochs=300 --lr=0.01 --evalmode="$EVALMODE"
+        ;;
+    NCI1)
+        python generate_ground_truth_graph_classification.py --dataset=NCI1 --output=nci1_dc --graph-mode --top_k=20 --disconnected --evalmode="$EVALMODE" &&
+        python explainer_gae_graph.py --distillation=nci1_dc_top20 --output=nci1_dc_top20 --dataset=NCI1 --gpu -b 128 --weighted --gae3 --loss=mse --early_stop --graph_labeling --train_on_positive_label --epochs=300 --lr=0.01 --evalmode="$EVALMODE"
+        ;;
+    IsCyclic)
+        python scripts/exp_node_tree_grids.py --alp="$ALP" --output="$FOLDER" > "$FOLDER"/log.txt
+        ;;
+    *)
+        echo "Something's wrong" >&2
+        exit 1
+        ;;
+esac
+
+python tests/baselines_graph.py "$DATASET" "$EVALMODE"
+
+exit 0
